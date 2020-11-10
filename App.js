f4e9063e7f8c16d99f5946db27859cfd70ccc8e8
Sazzad Islam <net_sazzad@outlook.com>
2020-11-10 22:45:22 +0600

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from "react";
+import { StyleSheet } from "react-native";
+import Mapview, { Marker } from "react-native-maps";
 
-export default function App() {
+function App() {
+  const [latitude, setLatitude] = useState(23.82235);
+  const [longitude, setLongitude] = useState(90.365417);
+  const [latitudeDelta, setLatitudeDelta] = useState(0.0922);
+  const [longitudeDelta, setLongitudeDelta] = useState(0.0421);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Mapview
+      style={styles.map}
+      initialRegion={{
+        latitude: latitude,
+        longitude: longitude,
+        latitudeDelta: latitudeDelta,
+        longitudeDelta: longitudeDelta,
+      }}
+    >
+      <Marker
+        coordinate={{
+          latitude: 23.82235,
+          longitude: 90.365417,
+        }}
+      />
+    </Mapview>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  map: {
+    position: "absolute",
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
   },
 });
+export default App;
